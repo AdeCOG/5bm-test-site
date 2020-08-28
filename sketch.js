11885f483ed13575b978d4af1af047ef3d39a329
rob <robertazzopardi@icloud.com>
2020-08-28 16:39:48 +0100

    added comments for the side bar js and put the code into a region

--- sketch.js
+++ sketch.js
@@ -3,6 +3,10 @@ var basketItems = [];
 const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
 const currency = "£";
 
+//#region SIDEBAR CODE
+
+// temp method to initilise basket items
+// remove with database support
 temp();
 function temp() {
     for (let index = 0; index < 10; index++) {
@@ -10,6 +14,7 @@ function temp() {
     }
 }
 
+// opens the side bar when the cart button is clicked 
 function openSideBar() {
     var sideNav = document.getElementById("mySidenav");
     sideNav.style.width = "250px";
@@ -27,12 +32,11 @@ function openSideBar() {
             // row div
             const row = document.createElement('div');
 
-
-            // name column
+            // name div
             const nameCol = document.createElement('div');
             nameCol.className = 'columnC';
 
-
+            // name text
             const nameText = document.createElement('p');
             nameText.innerText = `${element.name}`;
             nameText.style = 'color: white; margin-left:14px';
@@ -40,10 +44,11 @@ function openSideBar() {
 
             row.appendChild(nameCol);
 
-
+            // price div
             const priceCol = document.createElement('div');
             priceCol.className = 'columnC';
 
+            // price text
             const priceText = document.createElement('p');
             priceText.innerText = `${currency + element.price.toFixed(2)}`;
             priceText.style = 'text-align: center;color: white';
@@ -52,18 +57,22 @@ function openSideBar() {
             row.appendChild(priceCol);
 
             cartItems.appendChild(row);
-
         });
 
     } else {
+        // message to display when there are no items in the cart
         cartItems.innerText = "You have no items in your cart"
         cartItems.style.textAlign = "center";
     }
+
+    // update the total text box
     document.getElementById('totalItems').innerText = `Total: ${currency}${basketItems.reduce(reducer, 0).toFixed(2)}`;
 }
 
+// close the side bar when the side bar's x button is clicked 
 function closeSideBar() {
     document.getElementById("mySidenav").style.width = "0";
 
     document.body.style.backgroundColor = "white";
 }
+//#endregion
